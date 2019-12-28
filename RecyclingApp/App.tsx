5cd58af9d3f67cf7f458aa98059b475482c5f6a3
Peter Mulholland <pem42@pitt.edu>
2019-12-28 17:41:33 -0500

    Add icons

--- RecyclingApp/App.tsx
+++ RecyclingApp/App.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 import { ScannerScreen } from './screens/ScannerScreen';
 import { CheckListScreen } from './screens/CheckListScreen'
 
-const TabNavigator = createBottomTabNavigator({
-    ScannerScreen,
-    CheckListScreen,
-})
+const TabNavigator = createBottomTabNavigator(
+    {
+        ScannerScreen,
+        CheckListScreen,
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName: string = 'question';
+                if (routeName === 'ScannerScreen') {
+                    iconName = 'barcode';
+                } else if (routeName === 'CheckListScreen') {
+                    iconName = 'check-square-o';
+                }
+
+                // You can return any component that you like here!
+                return <Icon name={iconName} size={25} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'green',
+            inactiveTintColor: 'gray',
+        },
+    }
+);
 
 export default createAppContainer(TabNavigator);
 
