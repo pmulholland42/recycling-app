1906ab4f4b54aa96d556199a1af8f093fc6a1674
Peter Mulholland <pem42@pitt.edu>
2020-05-16 18:28:14 -0400

    Added cachify

--- RecyclingApp/utilities/api.ts
+++ RecyclingApp/utilities/api.ts
@@ -1,8 +1,14 @@
 import firestore from '@react-native-firebase/firestore';
 import Material from 'interfaces/Material';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export function getMaterials(): Promise<Material[]> {
-    return firestore().collection('materials').get().then(snapshot => {
+const MATERIALS_COLLECTION = 'materials';
+const MATERIALS_STORAGE_KEY = 'materials';
+
+export const getMaterials = cachify(fetchMaterialsFromFirestore, MATERIALS_STORAGE_KEY);
+
+function fetchMaterialsFromFirestore(): Promise<Material[]> {
+    return firestore().collection(MATERIALS_COLLECTION).get().then(snapshot => {
         return snapshot.docs.map(doc => {
             let data = doc.data();
             let material: Material = {
@@ -14,5 +20,20 @@ export function getMaterials(): Promise<Material[]> {
             };
             return material;
         });
-    })
+    });
+}
+
+function cachify<T extends (...args: any[]) => Promise<any>>(apiCall: T, storageKey: string) {
+    return function(...args: Parameters<T>): ReturnType<T> {
+        return AsyncStorage.getItem(storageKey).then(localData => {
+            if (localData == null) {
+                return apiCall(...args).then(fetchedData => {
+                    AsyncStorage.setItem(storageKey, JSON.stringify(fetchedData));
+                    return fetchedData;
+                });
+            } else {
+                return JSON.parse(localData);
+            }
+        }) as ReturnType<T>
+    }
 }
